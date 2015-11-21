b3aad4ccd022ae5bffacca6391ee85e275adc0d5
Mark Gomes <ellyosmark@gmail.com>
2015-11-21 15:54:14 -0300

    Configuracao do mapa
    alias key: alias_name

--- LocalizadorRadar/app/src/main/java/com/example/ios/localizadorradar/MainActivity.java
+++ LocalizadorRadar/app/src/main/java/com/example/ios/localizadorradar/MainActivity.java
@@ -1,18 +1,81 @@
 package com.example.ios.localizadorradar;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("App:", location.getLatitude() + ":" + location.getLongitude());
+                if (map != null){
+                    LatLng actualPosition = new LatLng(location.getLatitude() , location.getLongitude());
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(actualPosition)
+                            .zoom(17)
+                            .bearing(90)
+                            .tilt(30)
+                            .build();
+
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+
+                    map.addMarker(new MarkerOptions().position(new LatLng(-3,-5)).icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))));
+
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -35,5 +98,27 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+
+        map.setMyLocationEnabled(true);
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //Log.i("App:", location.getLatitude() + ":" + location.getLongitude());
+        if (location != null){
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                    .zoom(17)
+                    .bearing(90)
+                    .tilt(30)
+                    .build();
+
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+
     }
 }
