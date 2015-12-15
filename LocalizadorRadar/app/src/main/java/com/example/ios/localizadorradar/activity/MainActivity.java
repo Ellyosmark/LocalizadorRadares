729a72b1b66697a6e3051d72c3e96e69dc7b12b4
edgarpeixoto <edgarsaitou@gmail.com>
2015-12-14 23:32:37 -0200

    Update MainActivity.java

--- LocalizadorRadar/app/src/main/java/com/example/ios/localizadorradar/activity/MainActivity.java
+++ LocalizadorRadar/app/src/main/java/com/example/ios/localizadorradar/activity/MainActivity.java
@@ -1,14 +1,14 @@
-package com.example.ios.localizadorradar.activity;
+package com.localizadorradar;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.location.Criteria;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,18 +16,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.ios.localizadorradar.R;
-import com.example.ios.localizadorradar.dao.RadarDAO;
-import com.example.ios.localizadorradar.model.Radar;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.localizadorradar.dao.RadarDAO;
+import com.localizadorradar.model.Radar;
 
 import java.util.List;
 
@@ -45,33 +50,68 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
         this.radarDAO = new RadarDAO(this);
 
         this.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        findViewById(R.id.btnRadius).setOnClickListener(this);
+        //findViewById(R.id.btnRadius).setOnClickListener(this);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        SeekBar sb = (SeekBar) findViewById(R.id.seekBar);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                TextView progressBar = (TextView) findViewById(R.id.seekBarProgress);
+                progressBar.setText(""+progresValue);
+                //getMap();
+
+                Location location = map.getMyLocation();
+
+                if (map != null){
+                    LatLng actualPosition = new LatLng(location.getLatitude() , location.getLongitude());
+
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(actualPosition)
+                            .zoom(13)
+                            .bearing(90)
+                            .tilt(30)
+                            .build();
+
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    SeekBar sp = (SeekBar) findViewById(R.id.seekBar);
+                    List<Radar> radares = radarDAO.findAllByRadius(sp.getProgress(), location);
+                    map.clear();
+                    for(Radar r : radares){
+                        String nomeImagen = getNomeImagem(r);
+                        int idImage = getResources().getIdentifier(nomeImagen, "drawable", MainActivity.this.getPackageName());
+                        map.addMarker(new MarkerOptions().position(new LatLng(r.getLatitude(),r.getLongitude())).icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(getResources(), idImage))));
+                    }
+
+                    Circle circle = map.addCircle(new CircleOptions()
+                            .center(actualPosition)
+                            .radius(progresValue*1000)
+                            .strokeColor(Color.RED));
+
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //textView.setText("Covered: " + progress + "/" + seekBar.getMax());
+                //Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onMapReady(GoogleMap map) {
@@ -117,14 +157,14 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
 
     @Override
     public void onClick(View v) {
-        
-        if(((EditText)findViewById(R.id.editTextRadius)).getText().toString().equals(""))
-            return;
-        
+        //if(((EditText)findViewById(R.id.editTextRadius)).getText().toString().equals(""))
+          //  return;
+/*
         switch (v.getId()){
             case R.id.btnRadius:
                 getMap();
         }
+        */
     }
 
     private void getMap(){
@@ -142,8 +182,8 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
                             .build();
 
                     map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                    EditText editTextRadius = (EditText) findViewById(R.id.editTextRadius);
-                    List<Radar> radares = radarDAO.findAllByRadius(Float.parseFloat(editTextRadius.getText().toString()), location);
+                    SeekBar sp = (SeekBar) findViewById(R.id.seekBar);
+                    List<Radar> radares = radarDAO.findAllByRadius(sp.getProgress(), location);
                     map.clear();
                     for(Radar r : radares){
                         String nomeImagen = getNomeImagem(r);
@@ -172,5 +212,6 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 10, locationListener);
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        Log.i("haha","hehe");
     }
 }
